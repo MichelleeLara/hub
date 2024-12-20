6fec28ba37897f674ccb49f29b03f3628343bd4e
MichelleeLara <Mich.IA@hotmail.com>
2024-12-19 18:20:19 -0600

    feature:(Add Stack section )

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			screens: {
+				// Agregando breakpoints personalizados
+				'xs': '410px',  // Dispositivos pequeños
+				'sm': '640px',  // Sobreescribir el predeterminado si lo necesitas
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'2xl': '1536px',
+				// Breakpoint adicional
+				'3xl': '1920px',
+			},
 			colors: {
 				'primary': "#148cfa",
 				'secondary': "#F6F6F6", 
