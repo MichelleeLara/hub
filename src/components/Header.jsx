b1a676a1b1f880150bdc4b6a0f6479d4068f4905
MichelleeLara <Mich.IA@hotmail.com>
2025-01-04 19:20:43 -0600

    change color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -98,7 +98,7 @@ const Header = () => {
           animate={currentVariant}
         >
           <div className="mx-6 flex gap-3 items-center justify-between">
-            <div className="flex gap-3 items-center min-w-[195px] bg-red-50 ">
+            <div className="flex gap-3 items-center min-w-[175px] bg-gray-600 ">
               <img src="/author.png" className="w-12 object-top" alt="Author" />
               <AnimatePresence>
                 <div className="flex flex-col gap-0">
